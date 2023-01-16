20fb425bc3535684d86b31a1930de3c90734a016
etzinis <etzinis@gmail.com>
2023-01-16 04:50:56 -0600

    Unsupervised remixit dataset setup

--- baseline/utils/dataset_setup.py
+++ baseline/utils/dataset_setup.py
@@ -6,13 +6,16 @@
 """
 
 import baseline.dataset_loaders.libri1to3mix as libri1to3mix
+import baseline.dataset_loaders.chime as chime
 
 
 def create_loader_for_simple_dataset(dataset_name=None,
                                      hparams=None,
                                      fixed_n_sources=None,
                                      n_speakers_priors=None,
-                                     split=None):
+                                     split=None,
+                                     get_only_active_speakers=None,
+                                     n_samples=-1):
     if dataset_name == 'libri1to3mix':
         if split == "train":
             this_dataset_split = "train-360"
@@ -27,6 +30,19 @@ def create_loader_for_simple_dataset(dataset_name=None,
             min_or_max=hparams['min_or_max'], split=this_dataset_split,
             normalize_audio=False, n_samples=-1,
             n_speakers_priors=n_speakers_priors)
+    elif dataset_name == 'chime':
+        if split == "test":
+            this_dataset_split = "eval"
+        elif split == "val":
+            this_dataset_split = "dev"
+        else:
+            this_dataset_split = split
+        data_loader = chime.Dataset(
+            sample_rate=hparams['fs'], fixed_n_sources=fixed_n_sources,
+            timelength=hparams['audio_timelength'],
+            augment='train' in split, zero_pad=True,
+            get_only_active_speakers=get_only_active_speakers, split=this_dataset_split,
+            normalize_audio=False, n_samples=n_samples)
     else:
         raise ValueError('Dataset: {} is not yet supported!'.format(
             dataset_name))
@@ -75,4 +91,45 @@ def supervised_setup(hparams):
                 generators[this_dataset_name] = data_loader.get_generator(
                     batch_size=hparams['batch_size'], num_workers=hparams['n_jobs'])
 
+    return generators
+
+
+def unsupervised_setup(hparams):
+    # Create all generators
+    generators = {}
+    for data_split in ['train', 'val', 'test']:
+        if hparams[data_split] is None:
+            generators[data_split] = None
+            continue
+
+        if len(hparams[data_split]) > 1:
+            raise ValueError('Current implementation does not support '
+                             'training using multiple datasets.')
+
+        dataset_name = hparams[data_split][0]
+
+        if data_split == "train":
+            data_loader = create_loader_for_simple_dataset(
+                    dataset_name=hparams[data_split][0],
+                    hparams=hparams,
+                    fixed_n_sources=-1,
+                    get_only_active_speakers=False,
+                    split=data_split)
+            this_dataset_name = data_split
+            generators[this_dataset_name] = data_loader.get_generator(
+                batch_size=hparams['batch_size'], num_workers=hparams['n_jobs'])
+        else:
+            # This is only for validation and testing
+            for fixed_n_sources in [1]:
+                data_loader = create_loader_for_simple_dataset(
+                    dataset_name=hparams[data_split][0],
+                    hparams=hparams,
+                    fixed_n_sources=fixed_n_sources,
+                    get_only_active_speakers=False,
+                    split=data_split,
+                    n_samples=150)
+
+                this_dataset_name = f"{data_split}_{dataset_name}_{fixed_n_sources}sp"
+                generators[this_dataset_name] = data_loader.get_generator(
+                    batch_size=hparams['batch_size'], num_workers=hparams['n_jobs'])
     return generators
